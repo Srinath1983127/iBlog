5deed9c5cd414a89078a1dba97b8eab0b4c969f4
Srinath1983127 <92372563+Srinath1983127@users.noreply.github.com>
2021-10-12 17:14:46 +0530

    2021-12-07

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iBlog.Startup))]
+namespace iBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
